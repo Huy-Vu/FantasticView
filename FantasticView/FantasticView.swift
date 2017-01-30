a2c8802c4f828d4932ee7f8f40ee7f2977e69338
Huy Vu <rainhuygens@gmail.com>
2017-01-30 14:43:48 -0600

    make it public

--- FantasticView/FantasticView.swift
+++ FantasticView/FantasticView.swift
@@ -13,7 +13,7 @@ public class FantasticView: UIView {
     let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .gray]
     var colorCounter = 0
     
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         
         let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
@@ -26,7 +26,7 @@ public class FantasticView: UIView {
         scheduledColorChanged.fire()
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 }
